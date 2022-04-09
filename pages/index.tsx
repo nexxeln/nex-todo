3734670006c7e07fc853b5751d6efb23d61320e0
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-09 18:18:16 +0530

    render header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from "next";
 import { useState, useEffect, SetStateAction, Dispatch } from "react";
-import { Header, Title } from "@mantine/core";
 import { Session } from "@supabase/gotrue-js";
 import { supabase } from "../utils/supabaseClient";
 import Auth from "../components/Auth";
 import App from "../components/App";
+import Header from "../components/Header";
 
 type sessionState = [Session | null, Dispatch<SetStateAction<Session | null>>];
 
@@ -19,14 +19,9 @@ const Home: NextPage = () => {
     });
   }, []);
 
-  console.log("session", session);
   return (
     <>
-      <Header height="">
-        <Title order={1} align="center">
-          NEX TODO
-        </Title>
-      </Header>
+      <Header />
       {!session ? <Auth /> : <App />}
     </>
   );
