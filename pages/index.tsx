732549636b0763c468f35d5db78bbb896effff0e
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-09 16:51:18 +0530

    implement auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
-import { Header, Title } from "@mantine/core";
 import type { NextPage } from "next";
+import { useState, useEffect, SetStateAction, Dispatch } from "react";
+import { Header, Title } from "@mantine/core";
+import { supabase } from "../utils/supabaseClient";
+import Auth from "../components/Auth";
+import { Session } from "@supabase/gotrue-js";
+
+type sessionState = [Session | null, Dispatch<SetStateAction<Session | null>>];
 
 const Home: NextPage = () => {
+  const [session, setSession]: sessionState = useState(null as Session | null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <Header height="">
-      <Title order={1} align="center">
-        NEX TODO
-      </Title>
-    </Header>
+    <>
+      <Header height="">
+        <Title order={1} align="center">
+          NEX TODO
+        </Title>
+      </Header>
+      {!session && <Auth />}
+    </>
   );
 };
 
